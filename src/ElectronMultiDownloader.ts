4b4d4b18eb7a0693ba58c691c2f4b7aebc74a7f0
Theo Gravity <theo@switchboard.app>
2024-03-20 12:55:16 -0700

    Refactor

--- src/ElectronMultiDownloader.ts
+++ src/ElectronMultiDownloader.ts
@@ -1,119 +1,282 @@
 import crypto from 'crypto'
-import type { DownloadItem, DownloadURLOptions, Event, WebContents } from 'electron'
-import { DownloadManagerConfig, DownloadManagerItem } from './types'
+import type { BrowserWindow, DownloadItem, Event, WebContents } from 'electron'
+import {
+  DownloadManagerCallbacks,
+  DownloadManagerConstructorParams,
+  DownloadManagerItem,
+  DownloadParams,
+} from './types'
 
 type DoneEventFn = (_event: Event, state: 'completed' | 'cancelled' | 'interrupted') => Promise<void>
 type UpdatedEventFn = (_event: Event, state: 'progressing' | 'interrupted') => Promise<void>
 
+interface ItemHandlerParams {
+  id: string
+  event: Event
+  item: DownloadItem
+  webContents: WebContents
+  callbacks: DownloadManagerCallbacks
+}
+
+function tId(id: string) {
+  return id.slice(0, 5)
+}
+
+function tUrl(url: string) {
+  if (url.length > 50) {
+    return url.slice(0, 50) + '...'
+  }
+  return url
+}
+
 /**
- * Enables download handling for a BrowserWindow.
+ * Enables handling downloads in Electron.
  */
 export class ElectronMultiDownloader {
-  protected config: DownloadManagerConfig
+  // WeakMap for auto-cleanup when the download is done
   protected downloadItems: WeakMap<DownloadItem, DownloadManagerItem>
+  // Reverse lookup for download items. Mainly used for operations like item cancellation
+  protected idToDownloadItems: Record<string, DownloadItem>
+  // Used to keep track of the listeners so we can un-listen after we're done
   protected listeners: WeakMap<
     DownloadItem,
     {
+      id: string
       done: DoneEventFn
       updated: UpdatedEventFn
     }
   >
+  protected logger: (message: string) => void
 
-  constructor(config: DownloadManagerConfig) {
+  constructor(params: DownloadManagerConstructorParams = {}) {
     this.downloadItems = new WeakMap()
     this.listeners = new WeakMap()
-    this.config = config
+    this.idToDownloadItems = {}
+    this.logger = params.debugLogger || (() => {})
+  }
 
-    config.window.webContents.session.on('will-download', this.onWillDownload)
+  protected log(message: string) {
+    this.logger(message)
   }
 
   /**
    * Starts a download. If saveDialogOptions has been defined in the config,
    * the saveAs dialog will show up first.
    */
-  download(url: string, options?: DownloadURLOptions) {
-    this.config.window.webContents.downloadURL(url, options)
+  download({ window, url, downloadURLOptions, callbacks, saveDialogOptions }: DownloadParams) {
+    this.log(`Registering download for url: ${tUrl(url)}`)
+    window.webContents.session.once('will-download', this.onWillDownload({ callbacks, saveDialogOptions }))
+    window.webContents.downloadURL(url, downloadURLOptions)
   }
 
-  protected onWillDownload = async (
-    _event: Event,
-    item: DownloadItem,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    _webContents: WebContents
-  ): Promise<void> => {
-    if (this.config.saveDialogOptions) {
-      item.setSaveDialogOptions(this.config.saveDialogOptions)
+  /**
+   * Cancels a download
+   */
+  cancelDownload(id: string) {
+    const item = this.idToDownloadItems[id]
+
+    if (item) {
+      this.log(`[${tId(id)}] Cancelling download`)
+      item.cancel()
+    } else {
+      this.log(`[${tId(id)}] Download not found for cancellation`)
     }
+  }
 
-    const id = this.calculateId(item)
+  /**
+   * Pauses a download
+   */
+  pauseDownload(id: string) {
+    const item = this.idToDownloadItems[id]
 
-    this.downloadItems.set(item, {
-      id,
-      percentCompleted: 0,
+    if (item) {
+      this.log(`[${tId(id)}] Pausing download`)
+      item.pause()
+    } else {
+      this.log(`[${tId(id)}] Download not found for pausing`)
+    }
+  }
+
+  /**
+   * Resumes a download
+   */
+  resumeDownload(id: string) {
+    const item = this.idToDownloadItems[id]
+
+    if (item && item.isPaused()) {
+      this.log(`[${tId(id)}] Resuming download`)
+      item.resume()
+    } else {
+      this.log(`[${tId(id)}] Download not found or is not in a paused state`)
+    }
+  }
+
+  /**
+   * Enables network throttling on a BrowserWindow. Settings apply to *all*
+   * transfers in the window, not just downloads. Settings may be persistent
+   * on application restart, so use `disableThrottle` to reset after you're done
+   * testing.
+   * @see https://chromedevtools.github.io/devtools-protocol/tot/Network/#method-emulateNetworkConditions
+   * @see https://github.com/electron/electron/issues/21250
+   */
+  static async throttleConnections(
+    window: BrowserWindow,
+    conditions: {
+      offline?: boolean
+      latency?: number
+      downloadThroughput?: number
+      uploadThroughput?: number
+      connectionType?: string
+      packetLoss?: number
+      packetQueueLength?: number
+      packetReordering?: number
+    }
+  ) {
+    const dbg = window.webContents.debugger
+    dbg.attach()
+    await dbg.sendCommand('Network.enable')
+    await dbg.sendCommand('Network.emulateNetworkConditions', conditions)
+  }
+
+  /**
+   * Disables network throttling on a BrowserWindow
+   */
+  static async disableThrottle(window: BrowserWindow) {
+    const dbg = window.webContents.debugger
+    dbg.attach()
+    await dbg.sendCommand('Network.enable')
+    await dbg.sendCommand('Network.emulateNetworkConditions', {
+      offline: false,
+      upload: -1,
+      download: -1,
+      latency: 0,
     })
+    dbg.detach()
+  }
 
-    if (this.config.onDownloadStarted && this.downloadItems.has(item)) {
-      await this.config.onDownloadStarted({
-        ...this.downloadItems.get(item)!,
-        item,
+  /**
+   * Handles when the user initiates a download action by adding the developer-defined
+   * listeners to the download item events.
+   */
+  protected onWillDownload({
+    callbacks,
+    saveDialogOptions,
+  }: {
+    callbacks: DownloadManagerCallbacks
+    saveDialogOptions?: Electron.CrossProcessExports.SaveDialogOptions
+  }) {
+    return async (event: Event, item: DownloadItem, webContents: WebContents): Promise<void> => {
+      if (saveDialogOptions) {
+        this.log(`Prompting save as dialog`)
+        item.setSaveDialogOptions(saveDialogOptions)
+      }
+
+      const id = this.calculateId(item)
+
+      this.log(`[${tId(id)}] Associating to ${item.getFilename()}`)
+      this.log(`[${tId(id)}] Initiating download item handlers`)
+
+      this.downloadItems.set(item, {
+        id,
+        percentCompleted: 0,
       })
+
+      this.idToDownloadItems[id] = item
+
+      if (callbacks.onDownloadStarted && this.downloadItems.has(item)) {
+        this.log(`[${tId(id)}] Calling onDownloadStarted`)
+        await callbacks.onDownloadStarted({
+          ...this.downloadItems.get(item)!,
+          item,
+          event,
+          webContents,
+        })
+      }
+
+      const updatedHandler = this.itemOnUpdated({
+        id,
+        event: event,
+        item: item,
+        webContents: webContents,
+        callbacks: callbacks,
+      })
+
+      const doneHandler = this.itemOnDone({
+        id,
+        event: event,
+        item: item,
+        webContents: webContents,
+        callbacks: callbacks,
+      })
+
+      this.listeners.set(item, {
+        id,
+        done: doneHandler,
+        updated: updatedHandler,
+      })
+
+      item.on('updated', updatedHandler)
+      item.once('done', doneHandler)
     }
-
-    const updatedHandler = this.itemOnUpdated(item)
-    const doneHandler = this.itemOnDone(item)
-
-    this.listeners.set(item, {
-      done: doneHandler,
-      updated: updatedHandler,
-    })
-
-    item.on('updated', updatedHandler)
-    item.once('done', doneHandler)
   }
 
-  protected itemOnUpdated = (item: DownloadItem) => {
+  protected itemOnUpdated({ id, event, item, webContents, callbacks }: ItemHandlerParams) {
     return async (_event: Event, state: 'progressing' | 'interrupted') => {
       switch (state) {
         case 'progressing': {
           this.updateProgress(item)
-          if (this.config.onDownloadProgress && this.downloadItems.has(item)) {
-            await this.config.onDownloadProgress({
-              ...this.downloadItems.get(item)!,
+          if (callbacks.onDownloadProgress && this.downloadItems.has(item)) {
+            const data = this.downloadItems.get(item)!
+
+            this.log(`[${tId(id)}] Calling onDownloadProgress ${data.percentCompleted}%`)
+
+            await callbacks.onDownloadProgress({
+              ...data,
               item,
+              event,
+              webContents,
             })
           }
           break
         }
         case 'interrupted': {
-          console.log('interrupted A')
+          this.log(`onUpdated interrupted ${tId(id)}`)
           break
         }
       }
     }
   }
 
-  protected itemOnDone = (item: DownloadItem) => {
+  protected itemOnDone({ id, event, item, webContents, callbacks }: ItemHandlerParams) {
     return async (_event: Event, state: 'completed' | 'cancelled' | 'interrupted') => {
       switch (state) {
         case 'completed': {
-          if (this.config.onDownloadCompleted && this.downloadItems.has(item)) {
-            await this.config.onDownloadCompleted({
+          if (callbacks.onDownloadCompleted && this.downloadItems.has(item)) {
+            this.log(`[${tId(id)}] Calling onDownloadCompleted`)
+
+            await callbacks.onDownloadCompleted({
               ...this.downloadItems.get(item)!,
               item,
+              event,
+              webContents,
             })
           }
           break
         }
         case 'cancelled':
-          if (this.config.onDownloadCancelled && this.downloadItems.has(item)) {
-            await this.config.onDownloadCancelled({
+          if (callbacks.onDownloadCancelled && this.downloadItems.has(item)) {
+            this.log(`[${tId(id)}] Calling onDownloadCancelled`)
+
+            await callbacks.onDownloadCancelled({
               ...this.downloadItems.get(item)!,
               item,
+              event,
+              webContents,
             })
           }
           break
         case 'interrupted':
-          console.log('interrupted B')
+          this.log(`[${tId(id)}] itemOnDone interrupted`)
           break
       }
 
@@ -139,8 +302,10 @@ export class ElectronMultiDownloader {
     const listeners = this.listeners.get(item)
 
     if (listeners) {
+      this.log(`[${tId(listeners.id)}] Cleaning up`)
       item.removeListener('updated', listeners.updated)
       item.removeListener('done', listeners.done)
+      delete this.idToDownloadItems[listeners.id]
       this.listeners.delete(item)
       this.downloadItems.delete(item)
     }
--- src/types.ts
+++ src/types.ts
@@ -1,29 +1,59 @@
-import type { BrowserWindow, DownloadItem, SaveDialogOptions } from 'electron'
+import type { DownloadItem, SaveDialogOptions, WebContents, Event, BrowserWindow } from 'electron'
 
 export interface DownloadManagerItem {
+  /**
+   * Generated id for the download
+   */
   id: string
+  /**
+   * The percentage of the download that has been completed
+   */
   percentCompleted: number
 }
 
 export interface DownloadManagerCallbackData extends DownloadManagerItem {
+  /**
+   * The Electron.DownloadItem. Use this to grab the filename, path, etc.
+   * @see https://www.electronjs.org/docs/latest/api/download-item
+   */
   item: DownloadItem
+  /**
+   * The Electron.WebContents
+   * @see https://www.electronjs.org/docs/latest/api/web-contents
+   */
+  webContents: WebContents
+  /**
+   * The Electron.Event
+   * @see https://www.electronjs.org/docs/latest/api/event
+   */
+  event: Event
 }
 
+/**
+ * The download has started
+ */
 export type DownloadStartedFn = (data: DownloadManagerCallbackData) => Promise<void>
+/**
+ * There is progress on the download
+ */
 export type DownloadProgressFn = (data: DownloadManagerCallbackData) => Promise<void>
+/**
+ * The download has been cancelled
+ */
 export type DownloadCancelledFn = (data: DownloadManagerCallbackData) => Promise<void>
+/**
+ * The download has completed
+ */
 export type DownloadCompletedFn = (data: DownloadManagerCallbackData) => Promise<void>
 
-export interface DownloadManagerConfig {
+export interface DownloadManagerConstructorParams {
   /**
-   * BrowserWindow instance
+   * If defined, will log out internal debug messages
    */
-  window: BrowserWindow
-  /**
-   * If defined, will show a save dialog when the user
-   * downloads a file
-   */
-  saveDialogOptions: SaveDialogOptions
+  debugLogger?: (message: string) => void
+}
+
+export interface DownloadManagerCallbacks {
   /**
    * When a download has started
    */
@@ -40,4 +70,16 @@ export interface DownloadManagerConfig {
    * When the download has been cancelled
    */
   onDownloadCancelled?: DownloadCancelledFn
+}
+
+export interface DownloadParams {
+  window: BrowserWindow
+  url: string
+  callbacks: DownloadManagerCallbacks
+  downloadURLOptions?: Electron.DownloadURLOptions
+  /**
+   * If defined, will show a save dialog when the user
+   * downloads a file
+   */
+  saveDialogOptions: SaveDialogOptions
 }
